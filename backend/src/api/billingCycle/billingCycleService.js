7c56439fd2413a6568a90f3a28e5dc9e24b3dfbc
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-30 16:40:01 +0000

    create api rest config

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -5,4 +5,15 @@ BillingCycle.methods(['get','post','put','delete'])
 // set validators to put method
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+//fix request get method
+BillingCycle.route('get', (req, res, next) => {
+    BillingCycle.find({}, (err, docs) => {
+        if(!err) {
+            res.json(docs)
+        } else {
+            res.status(500).json({errors: [error]})
+        }
+    })
+})
+
 module.exports = BillingCycle
