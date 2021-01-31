3742ecbcec26f8d0c4a02a2ad970434ddb89e8e4
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-31 10:54:00 +0000

    alow send numeric param to pagination

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -6,11 +6,14 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const cors = require('./cors')
+// allow send numeric param from uri to pagination
+const queryParser = require('express-query-int')
 
 // all request will pass for the middleware body parser
 server.use(bodyParser.urlencoded({ extended: true}))
 server.use(bodyParser.json())
 server.use(cors)
+server.use(queryParser())
 
 server.listen(port, function(){
     console.log(`MyMoneyManager - Backend - running on port ${port}.`)
