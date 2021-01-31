b5b5b362db50127a437d09f1dd11615cf984ce25
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-31 23:20:51 +0000

    create dashboard boxs

---
+++ frontend/src/common/widget/valueBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Grid from '../layout/grid'
+
+export default props => (
+    <Grid cols={props.cols}> 
+        <div className={`small-box bg-${props.color}`}> 
+            <div className='inner'> 
+                <p>{props.text}</p>
+                <h3>{props.value}</h3>
+
+            </div> 
+            <div className='icon'> 
+                <i className={`fa fa-${props.icon}`}></i>
+            </div> 
+        </div> 
+    </Grid> 
+)
--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
-
+import ValueBox from '../common/widget/valueBox'
+import Row from  '../common/layout/row'
 
 class Dashboard extends Component {
 
@@ -9,10 +11,17 @@ class Dashboard extends Component {
 
         return (
             <div> 
-                <ContentHeader title='Dashboard' small='Versão 1.0' />
+                <ContentHeader title='Dashboard' small='version 1.0' />
                 <Content>
-                    Dashboard
-                </Content> 
+                   <Row>
+                       <ValueBox cols='12 4' color='light-blue' icon='bank'
+                          text='Total Credit'  value={`R$ 500,00`} />
+                       <ValueBox cols='12 4' color='red' icon='credit-card'
+                          text='Total Debit' value={`R$ 200,00`} />
+                       <ValueBox cols='12 4' color='green' icon='money'
+                          text='Total Available' value={`R$ 300,00`} />
+                   </Row>
+                </Content>
             </div>
         )
     }
