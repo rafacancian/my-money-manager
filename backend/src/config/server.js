c4fb9244dd14af097bc26cf12b3d8992bacc82e9
Rafael Cancian <cancian.rafa@gmail.com>
2021-01-31 00:09:17 +0000

    set allow cors origen

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -5,10 +5,12 @@ const bodyParser = require('body-parser')
 // singleton to create express
 const express = require('express')
 const server = express()
+const cors = require('./cors')
 
 // all request will pass for the middleware body parser
 server.use(bodyParser.urlencoded({ extended: true}))
 server.use(bodyParser.json())
+server.use(cors)
 
 server.listen(port, function(){
     console.log(`MyMoneyManager - Backend - running on port ${port}.`)
