17ebb55b0d949f5c494d9e386212e7330f2b19f0
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-02 23:15:18 +0000

    create tab header crud operations

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -5,6 +5,7 @@ import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
 
 class BillingCycle extends Component {
 
@@ -14,7 +15,12 @@ class BillingCycle extends Component {
                 <ContentHeader title='Payments cycle manager' small='Create payment' />
                 <Content> 
                     <Tabs>
-                        <TabsHeader>Header</TabsHeader>
+                        <TabsHeader>
+                            <TabHeader label='List' icon='bars' target='tabList' />
+                            <TabHeader label='Create' icon='plus' target='tabCreate' />
+                            <TabHeader label='Update' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Delete' icon='trash-o' target='tabDelete' />
+                        </TabsHeader>
                         <TabsContent>Content</TabsContent>
                     </Tabs>
                 </Content> 
