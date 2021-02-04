0716f8faff7fd0a8d059156d2fa8a1439206ccd0
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-04 23:48:57 +0000

    create billingCycleList component redux

---
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { getList, showUpdate, showDelete } from './billingCycleActions'
+
+class BillingCycleList extends Component {
+
+    render() {
+        return (
+            <div>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Mês</th>
+                            <th>Ano</th>
+                            <th className='table-actions'>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        body
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default BillingCycleList
