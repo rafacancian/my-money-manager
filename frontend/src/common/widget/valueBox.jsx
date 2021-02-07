a0ee420ccf2b2e0eb5fc41371d74cbb7bbe96726
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-07 11:21:55 +0000

    fix padding layout valuebox

--- frontend/src/common/widget/valueBox.jsx
+++ frontend/src/common/widget/valueBox.jsx
@@ -3,7 +3,7 @@ import Grid from '../layout/grid'
 
 export default props => (
     <Grid cols={props.cols}> 
-        <div className={`small-box bg-${props.color}`}> 
+        <div className={`small-box bg-${props.color} valueBox`}>
             <div className='inner'> 
                 <p>{props.text}</p>
                 <h3>{props.value}</h3>
