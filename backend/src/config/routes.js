5315f1d555f846524e994bc547191182faf18603
Rafael Cancian <cancian.rafa@gmail.com>
2021-02-10 22:48:51 +0000

    set private and open routers

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const auth = require('./auth')
 
 //set parameters between modules
 module.exports = function(server){
 
-    //set base URL to all routes
-    const router = express.Router()
-    server.use('/api', router)
+    // Routes protected from JWT
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
 
     //Routers to billing cycles
     const BillingCycle = require('../api/billingCycle/billingCycleService')
     BillingCycle.register(router, '/billingCycles')
+
+    // Routes open
+    const openApi = express.Router()
+    server.use('oapi', openApi)
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
